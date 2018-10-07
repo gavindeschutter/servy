1a2100c1107b3b97776e52c2c7d0dd102dd4efbb
Gavin DeSchutter <gavin.deschutter@mac.com>
2018-10-07 13:10:18 -0700

    rewrite file handling

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -52,21 +52,28 @@ defmodule Servy.Handler do
   end
 
   def route(%{ method: "GET", path: "/about" } = conv) do
-    file =
-      Path.expand("../../pages/", __DIR__)
-      |> Path.join("about.html")
-
-    case File.read(file) do
-      { :ok, content } ->
-        %{ conv | status: 200, resp_body: content }
-
-      { :error, :enoent } ->
-        %{ conv | status: 404, resp_body: "File not found" }
-
-      { :error, reason } ->
-        %{ conv | status: 400, resp_body: "File error: #{reason}" }
-    end
+    Path.expand("../../pages/", __DIR__)
+    |> Path.join("about.html")
+    |> File.read
+    |> handle_file(conv)
   end
+
+  # def route(%{ method: "GET", path: "/about" } = conv) do
+  #   file =
+  #     Path.expand("../../pages/", __DIR__)
+  #     |> Path.join("about.html")
+
+  #   case File.read(file) do
+  #     { :ok, content } ->
+  #       %{ conv | status: 200, resp_body: content }
+
+  #     { :error, :enoent } ->
+  #       %{ conv | status: 404, resp_body: "File not found" }
+
+  #     { :error, reason } ->
+  #       %{ conv | status: 400, resp_body: "File error: #{reason}" }
+  #   end
+  # end
 
   def route(%{ path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!" }
