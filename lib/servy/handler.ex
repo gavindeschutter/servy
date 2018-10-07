885565874170c142ebdf9ae7d69c46a3f5bff916
Gavin DeSchutter <gavin.deschutter@mac.com>
2018-10-07 12:25:02 -0700

    introudce status_reason. p after def denotes privacy of method usage in module

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -49,6 +49,17 @@ defmodule Servy.Handler do
     #{conv.resp_body}
     """
   end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
 end
 
 request = """
