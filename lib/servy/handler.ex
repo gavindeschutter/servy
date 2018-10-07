f1a7f4a7f030eb666eb64c4b73151ac56afd5551
Gavin DeSchutter <gavin.deschutter@mac.com>
2018-10-07 13:14:59 -0700

    write hande_file fns

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -58,6 +58,18 @@ defmodule Servy.Handler do
     |> handle_file(conv)
   end
 
+  def handle_file({ :ok, content }, conv) do
+    %{ conv | status: 200, resp_body: content }
+  end
+
+  def handle_file({ :error, :enoent }, conv) do
+    %{ conv | status: 404, resp_body: "File not found" }
+  end
+
+  def handle_file({ :error, reason  }, conv) do
+    %{ conv | status: 400, resp_body: "File error: #{reason}" }
+  end
+
   # def route(%{ method: "GET", path: "/about" } = conv) do
   #   file =
   #     Path.expand("../../pages/", __DIR__)
