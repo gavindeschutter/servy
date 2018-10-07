d243bbdee8b596a510e069c8af0d542779dd6a91
Gavin DeSchutter <gavin.deschutter@mac.com>
2018-10-07 13:02:03 -0700

    hand dandy case statement with pattern matching

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -56,8 +56,16 @@ defmodule Servy.Handler do
   end
 
   def route(%{ method: "GET", path: "/about" } = conv) do
-    {:ok, content} = File.read("../../pages/about.html")
-    %{ conv | status: 200, resp_body: content }
+    case File.read("../../pages/about.html") do
+      { :ok, content } ->
+        %{ conv | status: 200, resp_body: content }
+
+      { :error, :enoent } ->
+        %{ conv | status: 404, resp_body: "File not found" }
+
+      { :error, reason } ->
+        %{ conv | status: 400, resp_body: "File error: #{reason}" }
+    end
   end
 
   def route(%{ path: path } = conv) do
