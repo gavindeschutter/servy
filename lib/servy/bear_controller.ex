f684dbe1823037895881cbb604d5eb37119b55c6
Gavin DeSchutter <gavin.deschutter@mac.com>
2018-10-07 20:35:58 -0700

    add create fn to bear controller

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -6,4 +6,9 @@ defmodule Servy.BearController do
   def show(conv, %{ "id" => id }) do
     %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
+
+  def create(conv, %{ "name" => name, "type" => type }) do
+    %{ conv | status: 201,
+              resp_body: "Create a #{type} bear named #{name}!" }
+  end
 end
